ca1408ccef7c0179493971055755c289da4be905
Tazzy <tanlinsir@outlook.com>
2023-01-25 15:00:43 +0000

    Added database project. Not sure if it works

---
+++ SQL Database for Coursework/AccountsTable.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Accounts]
+(
+	[Username] VARCHAR(16) NOT NULL PRIMARY KEY, 
+    [Password] VARCHAR(MAX) NOT NULL
+)
---
+++ SQL Database for Coursework/VerticesTable.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Vertices]
+(
+	[VertexID] INT NOT NULL PRIMARY KEY,
+	[x] FLOAT,
+	[y] FLOAT,
+	[z] FLOAT
+
+)
