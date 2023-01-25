66ac0e8da25748ee632929b6d7da24dc17aa9ca9
Tazzy <67837437+Tazzy1907@users.noreply.github.com>
2023-01-25 19:07:39 +0000

    Delete SQL Database for Coursework directory

--- SQL Database for Coursework/AccountsTable.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].[Accounts]
-(
-	[Username] VARCHAR(16) NOT NULL PRIMARY KEY, 
-    [Password] VARCHAR(MAX) NOT NULL
-)
--- SQL Database for Coursework/VerticesTable.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Vertices]
-(
-	[VertexID] INT NOT NULL PRIMARY KEY,
-	[x] FLOAT,
-	[y] FLOAT,
-	[z] FLOAT
-
-)
